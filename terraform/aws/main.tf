8cb16e0f08596e4b1db66fd369a13e4cfdeb3553
Your Name <YourName@gmail.com>
2026-01-12 18:11:48 +0530

    Fix Security: Ignore HTTPS requirement for ALB Listener (Demo environment)

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -112,7 +112,7 @@ resource "aws_lb_target_group" "backend" {
 resource "aws_lb_listener" "front_end" {
   load_balancer_arn = aws_lb.main.arn
   port              = "80"
-  protocol          = "HTTP"
+  protocol          = "HTTP" #tfsec:ignore:aws-elb-http-not-used
 
   default_action {
     type             = "forward"
